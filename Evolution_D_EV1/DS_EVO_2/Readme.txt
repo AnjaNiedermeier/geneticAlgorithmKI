b3fbbe4e8256363ce799ee50561f65e0c36db80f
Anja Niedermeier <67066312+AnjaNiedermeier@users.noreply.github.com>
2024-04-08 16:01:37 +0200

    project setup

--- Evolution_D_EV1/DS_EVO_2/Readme.txt
+++
@@ -1,6 +0,0 @@
-Beachten Sie, dass alle Aufträge gleichzeitig vorliegen und die Zeit sofort losläuft (für Boni und Strafen).
-
-
-
-
-
---
+++ geneticAlgorithm/src/Beladungsstrategie.java
@@ -0,0 +1,8 @@
+public class Beladungsstrategie {
+    Auftrag[] auftraege;
+    Lkw[] lkws;
+
+    public static void main(String[] args) {
+        
+    }
+}
